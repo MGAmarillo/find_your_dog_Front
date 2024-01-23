419e23230bc3fd3d5594fa8da7c226567769cd07
MGAmarillo <mateo.amarillo50@gmail.com>
2024-01-22 21:43:59 -0300

    Upgrade home

--- find-your-dog/src/Home.jsx
+++ find-your-dog/src/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Logo from './assets/images/Find_your_dog_logo.png';
+import Mujer from './assets/images/Perro_mujer.png';
+import Veterinario from './assets/images/Veterinario.png';
+import Lupa from './assets/images/Lupa.png';
 import { Link } from 'react-router-dom';
 import './Home.css';
-
 
 const Home = () => {
   return (
     <div>
         <div className='navBar'>
-            <img src={Logo} alt="" />
+            <img src={Logo} alt="Logo Find Your Dog" />
             <Link to="/mapa">Mapa</Link>
             <Link to="/posts">Posts</Link>
             <Link to="/social">Social</Link>
@@ -18,17 +20,27 @@ const Home = () => {
             <h1>Encontrá a tu mascota,<br/>no importa dónde esté.</h1>
         </div>
         <div className='content'>
-          <div className='box'>
-            <p>Contenido de la caja 1</p>
-            <p>Más contenido de la caja 1</p>
+          
+          <div className='box firstBox'>
+            <pre>
+            <img src={Veterinario} alt="Imagen de un veterinario" />
+            <p>Encontrá veterinarias y <br/>
+              negocios. Podés validarlos<br/>
+              e incluso puntuarlos.</p>
+              </pre>
+            <button>Descubrilos</button>
           </div>
           <div className='box'>
-            <p>Contenido de la caja 2</p>
-            <p>Más contenido de la caja 2</p>
+            <img className='imagenPrincipal' src={Mujer} alt="Imagen de una mujer con su perro" />
           </div>
-          <div className='box'>
-            <p>Contenido de la caja 3</p>
-            <p>Más contenido de la caja 3</p>
+          <div className='box lastBox'>
+            <img src={Lupa} alt="Imagen de una lupa" />
+            <pre>
+            <p>Los usarios reportan y <br/>
+              y buscan a sus mascotas <br/>
+              en todo el país.</p>
+            </pre>
+            <button>Visitá el mapa</button>
           </div>
         </div>
     </div>
